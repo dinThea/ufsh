4f200b2dbf83ed763c3e8f632c8cc99a4dbf1886
perebaj <perebaj@gmail.com>
2019-08-22 15:03:56 -0300

    Documentação do maroto

--- commands/commands.c
+++ commands/commands.c
@@ -1,3 +1,6 @@
+/*
+@BRIEF: AÇÕES DOS COMANDOS QUE O INTERPRETADOR RECONHECE
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +11,12 @@ const char *FUNCTIONS[NUM_OPS] = {"CT", "RT", "AT", "LT", "IR", "BR", "AR", "RR"
 const int NUM_PARAMS[NUM_OPS] = {3, 2, 2, 1, 3, 4, 4, 2};
 const int (*functions[NUM_OPS]) (char *result, int num_args, char * argv[]) = {CT, RT, AT, LT, IR, BR, AR, RR};
 
-
+/*
+@BRIEF: Apaga o arquivo relativo a tabela e remove os dados de sua metabase
+@param  {result}    Resultado da operação
+@param  {num_args}  numero de argumentos que o interpretador reconhece}
+@param {argv}   armazenamento dos argumentes reconhecidos pelo interpretador
+*/
 int RT(char *result, int num_args, char *argv[]) {
     //RT nome_Da_Tabela
     printf ("Deletando tabela %s\n", argv[1]);
@@ -26,7 +34,12 @@ int RT(char *result, int num_args, char *argv[]) {
     return 0;
 
 }
-
+/*
+@BRIEF: Cria um arquivo vazio
+@param  {result}    Resultado da operação
+@param  {num_args}  numero de argumentos que o interpretador reconhece}
+@param {argv}   armazenamento dos argumentes reconhecidos pelo interpretador
+*/
 int CT(char *result, int num_args, char *argv[]) {
 //  CT CLIENTES INT:CODIGO;STR:NOME;BIN:CETIF  -- exemplo de entrada 
 //  comando nome_Da_Tabel campos
@@ -36,37 +49,40 @@ int CT(char *result, int num_args, char *argv[]) {
     result_Fields(argv[2], result);
 
 }
-
+/*
+@BRIEF: Apresenta um resumo dos metadados da tabela indicada: arquivos, campos e índices existentens.
+@param  {result}    Resultado da operação
+@param  {num_args}  numero de argumentos que o interpretador reconhece}
+@param {argv}   armazenamento dos argumentes reconhecidos pelo interpretador
+*/
 int AT(char *result, int num_args, char * argv[]) {
     
     return 0;
 }
+/*
+@BRIEF: Lista o nome de todas as tabelas existentes na base
+@param  {result}    Resultado da operação
+@param  {num_args}  numero de argumentos que o interpretador reconhece}
+@param {argv}   armazenamento dos argumentes reconhecidos pelo interpretador
+*/
 int LT(char *result, int num_args, char * argv[]) {
     return 0;
 }
 
-//Manipulaçao de resgistros nas tabelas
+/*
+@BRIEF: Insere um registro no arquivo de uma tabela já criada
+@param  {result}    Resultado da operação
+@param  {num_args}  numero de argumentos que o interpretador reconhece}
+@param {argv}   armazenamento dos argumentes reconhecidos pelo interpretador
+*/
 int IR(char *result, int num_args, char * argv[]) {
+
     //Exemplo: IR CLIENTES 10;JONATHAN;jonathan.crt
     printf("Inserindo Registro na tabela: %s \n", argv[1]);
-
-    char *registros[30];
-
-    char *delim = ";";
-    int registro_index = token(argv[2], registros, delim);
-
-
     printf("Com os Registros: \n");
 
-    for(int i = 0; i < registro_index; i++){
-        printf("%s", registros[i]);
-        if( i < registro_index - 1)
-            printf("\n");
-    }
+    result_Fields(argv[2], result);
 
-
-    
-    return 0;
 }
 int BR(char *result, int num_args, char * argv[]) {
     return 0;
@@ -104,16 +120,23 @@ int check_functions(char *op) {
 
 }
 
-
+/*
+@BRIEF  
+@param  {argv}      string que vai ser interpretada e delimitada
+@param  {fields}    alocação dos campos ou registros lidos em uma matriz de strings
+@param  {delim}     delimitador da string
+@return retorna o número de campos ou registros encontrados em {fields}
+*/
 int token(char *argv, char *fields[MAX_FIELDS], char *delim){
     
-    // char *delim = ";"; // delimitar a string
     char *pch;
     int field_index = 0; 
 
-    /*  index 0 : CT
-        index 1 nome da tabelaCT CLIENTES INT:CODIGO;STR:NOME;BIN:CETIF
-        index 2 
+    /*
+        CT CLIENTES INT:CODIGO;STR:NOME;BIN:CETIF  
+        index 0 : CT
+        index 1 : CLIENTES
+        index 2 : INT:CODIGO;STR:NOME;BIN:CETIF
     */
 
     pch = strtok(argv, delim);
@@ -133,7 +156,11 @@ int token(char *argv, char *fields[MAX_FIELDS], char *delim){
 
 }
 
-
+/*
+@BRIEF IMPRIME OS CAMPOS OU REGISTROS QUE FORAM VALIDADES PELO INTERPRETADOR 
+@param      {argv}      string de campo ou registro lido
+@param      {result}    resultado da validação feita
+*/
 void result_Fields(char argv[], char *result){
     char *fields[30];
     char *delim = ";";
