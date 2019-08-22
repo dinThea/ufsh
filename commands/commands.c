561a84f8eb8ecb5e6f3666883a2031a36da28ccb
perebaj <perebaj@gmail.com>
2019-08-22 19:23:09 -0300

    validação do input do usuário, para campos e registro

--- commands/commands.c
+++ commands/commands.c
@@ -8,7 +8,8 @@
 #include "../commands/commands.h"
 
 const char *FUNCTIONS[NUM_OPS] = {"CT", "RT", "AT", "LT", "IR", "BR", "AR", "RR"};
-const int NUM_PARAMS[NUM_OPS] = {3, 2, 2, 1, 3, 4, 4, 2};
+const int NUM_PARAMS[NUM_OPS] = {3, 2, 2, 1, 3, 4, 4, 2}; //quantidade mínima de parâmetros que cada comando deve ter(CT, RT, AT, LT, IR, BR, AR, RR)
+const int NUM_FIELDS[NUM_OPS] = {2, 1, 1, 0, 2, 2, 1, 1}; //quantidade mínima de compos que cada comando deve ter
 const int (*functions[NUM_OPS]) (char *result, int num_args, char * argv[]) = {CT, RT, AT, LT, IR, BR, AR, RR};
 
 /*
@@ -94,12 +95,18 @@ int RR(char *result, int num_args, char * argv[]) {
     return 0;
 }
 
+/*
+@BRIEF  Verifica a quantidade de parâmetros, se forem válidos chama a função que correspode ao primeiro comando lido(CT, RT, AT ...)
+@param      {resul}     Resultado da análise
+@param      {num_args}  Número de argumentos lidos
+@param      {argv}      Conteúdo de entrada
+@param      {func_index}    indice da função 
+*/
 int interpreter(char *result, int num_args, char *argv[], int func_index) {
-
-    if (num_args > NUM_PARAMS[func_index]) {
-        strcpy(result, "muitos argumentos");
-    } else if (num_args < NUM_PARAMS[func_index]) {
-        strcpy(result, "poucos argumentos");
+    if (num_args > NUM_PARAMS[func_index] || num_args < NUM_PARAMS[func_index]) {
+        strcpy(result, "número inválido de argumentos");
+    } else if (!valid_Fields(argv[num_args - 1], NUM_FIELDS[func_index])) {
+        strcpy(result,"sintax Error");
     } else {
         (*functions[func_index]) (result, num_args, argv);
     }
@@ -121,7 +128,7 @@ int check_functions(char *op) {
 }
 
 /*
-@BRIEF  
+@BRIEF  Separa a string de entrada com o delimitador passado por parâmetro
 @param  {argv}      string que vai ser interpretada e delimitada
 @param  {fields}    alocação dos campos ou registros lidos em uma matriz de strings
 @param  {delim}     delimitador da string
@@ -162,8 +169,8 @@ int token(char *argv, char *fields[MAX_FIELDS], char *delim){
 @param      {result}    resultado da validação feita
 */
 void result_Fields(char argv[], char *result){
-    char *fields[30];
-    char *delim = ";";
+    char *fields[30];// armazenamento dos campos ou registros lidos
+    char *delim = ";"; //delimitador da string de entrada
     int field_index = token(argv, fields, delim);
 
     for (int i = 0; i < field_index; i++) {
@@ -176,4 +183,19 @@ void result_Fields(char argv[], char *result){
 
     strcpy(result, " ");
 
+}
+/*
+@BRIEF  verifa se a quantidade de campos ou registros é valida para determinado comando( EX: CT tabela campo -- CT  NOVATABELA NOME:JONATHAN)  o mínimo é que tenha 2 campos separados por ":"
+@param      {argv}    string que vai ser verificada 
+@param      {num_Index}     Número mínimo de indices para a determinada tarefa(EX: CT- min 2 ; RT - min 1)
+*/
+int valid_Fields(char * argv, int num_Index){
+    char *fields[50];
+    char str_Aux[50];
+    strcpy(str_Aux, argv);
+
+    int index = token(str_Aux, fields, ":");
+    if (index < num_Index)
+        return 0;
+    return 1;
 }
